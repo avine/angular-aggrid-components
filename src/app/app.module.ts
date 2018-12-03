53590a20e1630fe45062b9c50706f5a51993d03c
Stéphane Francel <contact@avine.io>
2018-12-03 07:02:34 +0100

    Move `users` into `./components` folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AgGridVendorModule } from './ag-grid/ag-grid.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AgGridVendorModule } from './ag-grid/ag-grid.module';
-import { MaterialVendorModule } from './vendor/material.module';
-import { UsersComponent } from './users/users.component';
+import { UsersComponent } from './components/users/users.component';
+import { MaterialVendorModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
